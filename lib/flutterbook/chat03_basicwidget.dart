a25df00782a82203173a1c159e812edfce3b89e0
xialuo <v_xialuo@tinnove.com.cn>
2021-10-08 18:37:05 +0800

    文本样式

--- lib/flutterbook/chat03_basicwidget.dart
+++ lib/flutterbook/chat03_basicwidget.dart
@@ -14,17 +14,45 @@ class MyApp extends StatelessWidget {
       //BoxDecoration:控制背景颜色；Scaffold有一个默认的白色背景色和默认的黑色字体色，
       //如果没用scaffold的话，就是一个默认黑色的背景和字体颜色，所以需要自己配置
       decoration: new BoxDecoration(color: Colors.white),
-      child: new Center(
-        child: new Text(
-          "hello6660000000000000000",
-          textDirection: TextDirection.ltr,
-          style: new TextStyle(fontSize: 40, color: Colors.black87),
-          maxLines: 1,
-          textAlign: TextAlign.end,
-          overflow: TextOverflow.ellipsis,
-          textScaleFactor: 1.5,
-        ),
+      // child: new Center(
+      child: new Text(
+        "hello6660000000000000000",
+        textDirection: TextDirection.ltr,
+        maxLines: 1,
+        textAlign: TextAlign.end,
+        overflow: TextOverflow.ellipsis,
+        textScaleFactor: 1.5,
+
+        //字体样式
+        style: new TextStyle(
+            color: Colors.pink,
+            //fontSize同textScaleFactor一样，用于设置字体大小，不同的是：fontSize不随系统字体大小改变，textScaleFactor跟随系统字体大小改变
+            fontSize: 30.0,
+            height: 1.2,
+            fontFamily: "Courier",
+            background: Paint()..color = Colors.yellow,
+            //下划线
+            decoration: TextDecoration.underline,
+            //下划线是虚线，默认是实线
+            decorationStyle: TextDecorationStyle.dashed),
       ),
+      // ),
+    );
+  }
+
+}
+
+class TextSpanClass extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // throw UnimplementedError();
+
+    return new Container(
+      decoration: new BoxDecoration(color: Colors.white),
+      child: new Text.rich(TextSpan(children: [
+        TextSpan(text: "HOME"),
+        TextSpan(text: "https://xxx.com", style: TextStyle(color: Colors.blue))
+      ])),
     );
   }
 }
@@ -36,9 +64,9 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: new AppBar(title: new Text("hello ppx")),
       body: new Center(
           child: new Text(
-            "Hello 000",
-            style: new TextStyle(fontSize: 32.0),
-          )),
+        "Hello 000",
+        style: new TextStyle(fontSize: 32.0),
+      )),
     );
   }
 }
@@ -50,3 +78,4 @@ class MyHomePage extends StatefulWidget {
     throw UnimplementedError();
   }
 }
+
