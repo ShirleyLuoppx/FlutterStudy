3755dc04543a80621f96ba21519af01e807648cd
xialuo <v_xialuo@tinnove.com.cn>
2021-10-11 14:18:34 +0800

    提交几种Container容器的简单使用

--- lib/sevenday/daythree.dart
+++ lib/sevenday/daythree.dart
@@ -63,7 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: new AppBar(
         title: new Text(widget.title),
       ),
-      body: expandedTest,
+      body: combineContainerPaddingTest,
       floatingActionButton: new FloatingActionButton(
         backgroundColor: Colors.black12,
         elevation: 12,
@@ -84,9 +84,7 @@ class _MyHomePageState extends State<MyHomePage> {
   ///横向排列，如果内容超出一行会有一个黄色斜条纹提示
   var rowTest = Row(
     children: <Widget>[
-      Text(
-        "雕栏玉砌应犹在，"
-      ),
+      Text("雕栏玉砌应犹在，"),
       Text("只是朱颜改。"),
       Text("问君能有几多愁，"),
       Text("恰似一江春水向东流。")
@@ -123,4 +121,50 @@ class _MyHomePageState extends State<MyHomePage> {
   );
 
   ///Container--容器
+  //普通容器
+  var containerTest = new Container(
+    child: Text("this is  a Container Widget"),
+    color: Colors.white,
+    width: 200,
+    height: 200,
+    margin: EdgeInsets.fromLTRB(20, 20, 20, 10),
+    padding: EdgeInsets.all(20),
+  );
+
+  //居中容器
+  var centerContainerTest = new Center(
+    child: Text("this is a Center Container Test "),
+    heightFactor: 200,
+    widthFactor: 200,
+  );
+
+  //Padding容器，padding参数必传
+  var paddingContainerTest = new Padding(
+    padding: EdgeInsets.all(20),
+    child: Text("this is a Padding Container Test"),
+  );
+
+  //混合容器：Container+Center
+  var combineContainerCenterTest = new Container(
+    color: Colors.limeAccent,
+    width: 300,
+    height: 300,
+    child: Center(
+      child: Text("this is a combine container with Container and Center"),
+    ),
+  );
+
+  //混合容器：Container+Padding
+  var combineContainerPaddingTest = new Container(
+    color: Colors.purpleAccent,
+    width: 300,
+    height: 300,
+    child: Padding(
+      padding: EdgeInsets.all(10),
+      child:
+          Text("this is a combine container with Container and Padding widget"),
+    ),
+  );
+
+  var stack = new Stack();
 }
