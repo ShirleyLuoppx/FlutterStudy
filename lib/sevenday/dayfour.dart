de3333ac636c1279048d96eecd14bed9ae5a2851
xialuo <v_xialuo@tinnove.com.cn>
2021-10-21 14:36:56 +0800

    Flex布局剩余属性小例子

--- lib/sevenday/dayfour.dart
+++ lib/sevenday/dayfour.dart
@@ -46,7 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: new AppBar(
         title: new Text(widget.title),
       ),
-      body: mainAxisAlignmentTest,
+      body: textBaseLineFun(),
       floatingActionButton: new FloatingActionButton(
         backgroundColor: Colors.black12,
         elevation: 12,
@@ -64,16 +64,204 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   ///
+  /// textBaseLine：基线对齐方式（乍一看 确实看不出区别的..）
+  ///
+  var textBaseLine = [TextBaseline.alphabetic, TextBaseline.ideographic];
+  var redText = Text(
+    "hello",
+    style: TextStyle(fontSize: 20, backgroundColor: Colors.red),
+  );
+  var blueText = Text(
+    "convenient",
+    style: TextStyle(fontSize: 30, backgroundColor: Colors.blue),
+  );
+  var greenText = Text(
+    "Flutter",
+    style: TextStyle(fontSize: 10, backgroundColor: Colors.green),
+  );
+
+  textBaseLineFun() {
+    return MultiShower(
+      [TextBaseline.alphabetic],
+      (e) {
+        return Flex(
+          direction: Axis.horizontal,
+          children: [
+            redText,
+            blueText,
+            greenText,
+          ],
+        );
+      },
+      infos: [""],
+      width: 300,
+      height: 500,
+      color: Colors.black,
+    );
+  }
+
+  ///
+  /// verticalDirection：竖直方向排序
+  ///
+  var verticalDirections = [
+    //从上往下
+    VerticalDirection.down,
+    //下往上
+    VerticalDirection.up
+  ];
+
+  verticalDirectionFun() {
+    return MultiShower(
+      [VerticalDirection.down],
+      (e) {
+        return Flex(
+          direction: Axis.vertical,
+          verticalDirection: e,
+          children: [
+            redContainer,
+            blueContainer,
+            yellowContainer,
+            greenContainer
+          ],
+        );
+      },
+      infos: [""],
+      height: 300,
+      width: 500,
+      color: Colors.black,
+    );
+  }
+
+  ///
+  /// textDirection：文字方向
+  ///
+  var textDirection = [
+    //从左到右
+    TextDirection.ltr,
+    //右到左
+    TextDirection.rtl
+  ];
+
+  textDirectionFun() {
+    return MultiShower(
+      [TextDirection.rtl],
+      (e) {
+        return Flex(
+          direction: Axis.horizontal,
+          textDirection: e,
+          children: [
+            redContainer,
+            blueContainer,
+            yellowContainer,
+            greenContainer
+          ],
+        );
+      },
+      infos: [""],
+      width: 300,
+      height: 500,
+      color: Colors.white,
+    );
+  }
+
+  ///
+  /// mainAxisSize：主轴方向
+  ///
+  var mainAxisSize = [
+    //仅包裹自己就行，相当于android的wrap_content
+    MainAxisSize.min,
+    //当父容器的宽高未指定，Flex默认会将自身尽可能的延申，相当于android的match_parent
+    MainAxisSize.max
+  ];
+
+  mainAxisSizeFun() {
+    return Center(
+      child: Flex(
+        direction: Axis.horizontal,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          yellowContainer,
+          greenContainer,
+          blueContainer,
+          redContainer
+        ],
+      ),
+    );
+  }
+
+  ///
+  /// CrossAxisAlignment 交叉轴方向(竖直轴方向)
+  ///
+  var crossAxisAlignmentType = [
+    CrossAxisAlignment.start,
+    CrossAxisAlignment.center,
+    CrossAxisAlignment.end,
+    CrossAxisAlignment.stretch,
+    //交叉轴方向为baseline的时候，Flex必须包含textBaseline这个参数哟
+    CrossAxisAlignment.baseline
+  ];
+
+  crossAxisAlignmentFun() {
+    return MultiShower(
+      [CrossAxisAlignment.baseline],
+      (e) {
+        return Flex(
+          direction: Axis.horizontal,
+          crossAxisAlignment: e,
+          textBaseline: TextBaseline.alphabetic, //基线类型。看起来两个值没啥区别
+          children: [
+            redContainer,
+            blueContainer,
+            greenContainer,
+            yellowContainer
+          ],
+        );
+      },
+      infos: [""],
+      width: 300,
+      height: 500,
+    );
+  }
+
+  ///
   /// 主轴方向：MainAxisAlignment
   ///
   var mainAxisAlignmentDirections = [
+    //主轴水平顶头
     MainAxisAlignment.start,
+    //内容居中
     MainAxisAlignment.center,
+    //内容接尾
     MainAxisAlignment.end,
+    //顶头接尾，其他均分
     MainAxisAlignment.spaceBetween,
+    //中间均分，两边空一半
     MainAxisAlignment.spaceAround,
+    //均分
     MainAxisAlignment.spaceEvenly
   ];
+
+  mainAxisAlignmentFun() {
+    return MultiShower(
+      [MainAxisAlignment.spaceEvenly],
+      (e) {
+        return Flex(
+          direction: Axis.horizontal,
+          mainAxisAlignment: e,
+          children: <Widget>[
+            redContainer,
+            blueContainer,
+            yellowContainer,
+            greenContainer
+          ],
+        );
+      },
+      infos: [""],
+      width: 500,
+      height: 200,
+    );
+  }
+
   var mainAxisAlignmentTest = MultiShower(
     [MainAxisAlignment.start],
     (e) {
