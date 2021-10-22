67a07d2ae7d61ed8c5ebd8f857a6e42b26e51a4d
xialuo <v_xialuo@tinnove.com.cn>
2021-10-22 14:13:17 +0800

    添加布局测试小栗子2

--- lib/sevenday/dayfive.dart
+++ lib/sevenday/dayfive.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -43,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: new AppBar(
         title: new Text(widget.title),
       ),
-      body: test1Fun(),
+      body: test2Fun(),
       floatingActionButton: new FloatingActionButton(
         backgroundColor: Colors.black12,
         elevation: 12,
@@ -57,6 +59,57 @@ class _MyHomePageState extends State<MyHomePage> {
           semanticLabel: "hhaa",
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  test2Fun() {
+    return Padding(
+      padding: EdgeInsets.all(5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Image.asset(
+            "images/panda.png",
+            width: 50,
+            height: 50,
+          ),
+          Expanded(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 5,top: 5),
+                child: Text(
+                  "点绛唇",
+                  style: TextStyle(fontSize: 20, color: Colors.black),
+                  textAlign: TextAlign.end,
+                  textDirection: TextDirection.rtl,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10, left: 5),
+                child: Text(
+                  "溪壑分离，红尘游戏，真何趣？名利犹虚，后事终难继",
+                  style: TextStyle(fontSize: 15, color: Colors.grey),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
+                ),
+              )
+            ],
+          )),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              "昨天",
+              style: TextStyle(color: Colors.grey, fontSize: 13),
+            ),
+          )
+        ],
+      ),
     );
   }
 
@@ -85,5 +138,22 @@ class _MyHomePageState extends State<MyHomePage> {
       padding: EdgeInsets.all(10),
       color: Colors.white,
     );
+  }
+
+  //封装一个生成背景色块的方法
+  Container getBackGroundColor(Widget w, [Color? color]) {
+    return Container(
+      child: w,
+      color: color ?? getColor(),
+    );
+  }
+
+  Color getColor() {
+    Random random = Random();
+    int a = random.nextInt(200) + 30;
+    int r = random.nextInt(200) + 30;
+    int g = random.nextInt(200) + 30;
+    int b = random.nextInt(200) + 50;
+    return Color.fromARGB(a, r, g, b);
   }
 }
